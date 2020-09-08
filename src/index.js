742db8dc6075f9978e572149a092088c0c564596
Francisco Sepulveda <panchi_nrc@hotmail.com>
2020-09-08 19:22:25 -0300

    asd

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
+
+
+app.set('port', process.env.PORT || 3000);
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(require('./routes/index'))
+
+app.use(require('./routes/index.js'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
 
 
-app.use(express.static(path.join(__dirname, 'public')))
-
-
-
-
-app.use(express.json())
-app.listen(3000, () => {
-    console.log('Server on port 3000')
-})
