a2a61b1364646cdc3564eedd53d267b797b58e47
Francisco Sepulveda <panchi_nrc@hotmail.com>
2020-09-23 18:40:36 -0300

    Optimizacion seo

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-
-
-
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(require('./routes/index.js'));
@@ -15,7 +12,5 @@ app.use(require('./routes/index.js'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`);
+	console.log(`Server on port ${app.get('port')}`);
 });
-
- 
